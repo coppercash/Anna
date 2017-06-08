aff414c01b63aa0526972e4edf339674fea7584a
William <coderdreamer@gmail.com>
2017-06-09 07:54:41 +0800

    Untracked Pods

--- Pods/Target Support Files/Pods-Anna_iOS/Pods-Anna_iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Anna_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_Anna_iOS
-@end
--- Pods/Target Support Files/extobjc/extobjc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_extobjc : NSObject
-@end
-@implementation PodsDummy_extobjc
-@end
