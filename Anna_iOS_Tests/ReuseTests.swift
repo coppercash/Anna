2ab34983bcca8bb205fc58e1b5289996229d00e0
William <coderdreamer@gmail.com>
2018-06-03 22:11:00 +0800

    test_unowned_sub_analyzer_should_be_deregister_properly

--- Anna_iOS_Tests/ReuseTests.swift
+++ Anna_iOS_Tests/ReuseTests.swift
@@ -122,4 +122,115 @@ class ReuseTests: XCTestCase {
         XCTAssertEqual(test[0] as? Int, 10)
         XCTAssertEqual(test[1] as? Int, 21)
     }
+    
+    // This test case doesn't really test the function.
+    // It just constructs a case to view the log,
+    // so it is confirmed that the functoin works properly.
+    //
+    func
+        test_unowned_sub_analyzer_should_be_deregister_properly() {
+        let
+        test = PathTestCaseBuilder(with: self)
+        test.task = ("""
+        match(
+          'tb/rw/vc/ana-disappeared',
+          function(node) { return 42; }
+        );
+        """)
+        class
+            Controller : PathTestingViewController, UITableViewDelegate, UITableViewDataSource
+        {
+            var
+            counter = 10
+            lazy var
+            table :PathTestingTableView = {
+                let
+                superview = self.view!;
+                let
+                table = PathTestingTableView(frame: superview.bounds)
+                table.delegate = self
+                table.dataSource = self
+                table.register(
+                    PathTestingTableViewCell.self,
+                    forCellReuseIdentifier: "r"
+                )
+                return table
+            }()
+            override func
+                viewDidLoad() {
+                super.viewDidLoad()
+                self.analyzer.enable(with: "vc")
+                self.view.addSubview(self.table)
+                self.analyzer.setSubAnalyzer(
+                    self.table.analyzer,
+                    for: "tb"
+                )
+            }
+            override func
+                viewDidAppear(_ animated: Bool) {
+                super.viewDidAppear(animated)
+                self.table.delegate?.tableView?(
+                    self.table,
+                    didSelectRowAt: IndexPath(row: 0, section: 0)
+                )
+            }
+            func
+                tableView(
+                _ tableView: UITableView,
+                cellForRowAt indexPath: IndexPath
+                ) -> UITableViewCell {
+                let
+                cell = tableView.dequeueReusableCell(
+                    withIdentifier: "r",
+                    for: indexPath
+                ) as! PathTestingTableViewCell
+                cell.analyzer.enable(with: "rw")
+                return cell
+            }
+            func
+                tableView(
+                _ tableView: UITableView,
+                numberOfRowsInSection section: Int
+                ) -> Int {
+                return 20
+            }
+            func
+                tableView(
+                _ tableView: UITableView,
+                didSelectRowAt indexPath: IndexPath
+                ) {
+                let
+                controller = PathTestingViewController()
+                controller.analyzer.enable(with: "vc")
+                self.navigationController?.pushViewController(
+                    controller,
+                    animated: false
+                )
+                DispatchQueue.main.async {
+                    tableView.scrollToRow(
+                        at: IndexPath(row: 19, section: 0),
+                        at: .bottom,
+                        animated: false
+                    )
+                    DispatchQueue.main.async {
+                        self.navigationController?.popViewController(
+                            animated: false
+                        )
+                    }
+                }
+            }
+        }
+        test.rootViewController = UINavigationController(
+            rootViewController: Controller()
+        )
+
+        test.expect()
+        test.launch()
+        self.wait(
+            for: test.expectations,
+            timeout: 999.0
+        )
+        
+        XCTAssertEqual(test[0] as? Int, 42)
+    }
 }
