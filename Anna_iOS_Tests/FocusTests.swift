c13da0f11a68498242739ea88b65af41bdd530ca
William <coderdreamer@gmail.com>
2018-07-03 18:28:12 +0800

    restore the capability to handle focus marking on analyzer activiated with standalone name

--- Anna_iOS_Tests/FocusTests.swift
+++ Anna_iOS_Tests/FocusTests.swift
@@ -196,4 +196,118 @@ class FocusTests: XCTestCase {
         
         XCTAssertEqual(test[0] as? Int, 42)
     }
+    
+    func test_analyzer_activiated_with_standalone_name_should_handle_focus_marking_properly() {
+        let
+        test = PathTestCaseBuilder(with: self)
+        test.task = ("""
+        match(
+          'master/table/cell/button/detail/ana-appeared',
+          function(node) { return 42; }
+        );
+        """)
+        class
+            Controller : PathTestingViewController, UITableViewDelegate, UITableViewDataSource
+        {
+            class
+                Cell : PathTestingTableViewCell
+            {
+                override
+                init(
+                    style: UITableViewCellStyle,
+                    reuseIdentifier: String?
+                    ) {
+                    super.init(
+                        style: style,
+                        reuseIdentifier: reuseIdentifier
+                    )
+                    let
+                    cell = self,
+                    button = PathTestingButton(frame: cell.contentView.bounds)
+                    cell.contentView.addSubview(button)
+                    button.analyzer.enable(naming: "button")
+                    
+                    button.addTarget(
+                        nil,
+                        action: #selector(Controller.handleControlEvent),
+                        for: .touchUpInside
+                    )
+                }
+                required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
+            }
+            var
+            counter = 10
+            lazy var
+            table :PathTestingTableView = {
+                let
+                superview = self.view!;
+                let
+                table = PathTestingTableView(frame: superview.bounds)
+                table.delegate = self
+                table.dataSource = self
+                table.register(
+                    Cell.self,
+                    forCellReuseIdentifier: "r"
+                )
+                return table
+            }()
+            override func
+                viewDidLoad() {
+                super.viewDidLoad()
+                self.analyzer.enable(naming: "master")
+                self.view.addSubview(self.table)
+                self.table.analyzer.enable(naming: "table")
+            }
+            override func
+                viewDidAppear(_ animated: Bool) {
+                super.viewDidAppear(animated)
+                let
+                cell = self.table.cellForRow(
+                    at: IndexPath(row: 9, section: 0)
+                ),
+                button = cell?.contentView.subviews[0] as! PathTestingButton
+                button.analyzer.markFocused()
+                button.sendActions(for: .touchUpInside)
+            }
+            func
+                tableView(
+                _ tableView: UITableView,
+                cellForRowAt indexPath: IndexPath
+                ) -> UITableViewCell {
+                let
+                cell = tableView.dequeueReusableCell(
+                    withIdentifier: "r",
+                    for: indexPath
+                    ) as! PathTestingTableViewCell
+                cell.analyzer.enable(naming: "cell")
+                return cell
+            }
+            func
+                tableView(
+                _ tableView: UITableView,
+                numberOfRowsInSection section: Int
+                ) -> Int {
+                return 10
+            }
+            @objc func
+                handleControlEvent() {
+                let
+                detail = PathTestingViewController()
+                detail.analyzer.enable(naming: "detail")
+                self.navigationController?.pushViewController(detail, animated: false)
+            }
+        }
+        let
+        navigation = UINavigationController(rootViewController: Controller())
+        test.rootViewController = navigation
+
+        test.expect(for: 1)
+        test.launch()
+        self.wait(
+            for: test.expectations,
+            timeout: 1.0
+        )
+        
+        XCTAssertEqual(test[0] as? Int, 42)
+    }
 }
